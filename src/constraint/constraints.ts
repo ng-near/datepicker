baeea3e02c997241f2cfe15ad5880f4a7c800e48
ghetolay <ghetolay@gmail.com>
2018-04-28 17:26:25 +0200

    refactor: add more consistency on code

--- src/constraint/constraints.ts
+++ src/constraint/constraints.ts
@@ -1,3 +1,4 @@
+import { newDayDate } from '..';
 import { DayDate, isSameDay } from '../utils';
 
 // TODO can date be null ? undefined ?
@@ -5,25 +6,30 @@ export type DateConstraintFn = (date: DayDate) => {[key: string]: boolean} | nul
 
 export const DateConstraints = {
   minDate(minDate: Date): DateConstraintFn {
-    return date => date.getTime() > minDate.getTime() ? null : {
+    const minTime = newDayDate(minDate.getTime()).getTime();
+
+    return date => date.getTime() < minTime ? {
       minDate: true
-    };
+    } : null;
   },
 
   maxDate(maxDate: Date): DateConstraintFn {
-    return date => date.getTime() < maxDate.getTime() ? null : {
+    const maxTime = newDayDate(maxDate.getTime()).getTime();
+
+    return date => date.getTime() > maxTime ? {
       maxDate: true
-    };
+    } : null;
   },
 
   invalidDates(disabled: Date[]): DateConstraintFn {
-    return date => disabled.some( d => isSameDay(date, d) ) ? {
+    return date => disabled.some(d => isSameDay(date, d)) ? {
       disabledDates: true
     } : null;
   },
 
-  // TODO use locale to define weekend ?
-  InvalidateWeekend(date: Date) {
+  // TODO use locale to define weekend
+  invalidateWeekend(date: Date, locale: string) {
+    // const {day1, day2} = getLocaleWeekEndRange(locale);
     return date.getDay() !== 0 && date.getDay() !== 6 ? null : {
       disabledWeekend: true
     };
