a28a152fe498071bae33062d28ca628898e6151e
ghetolay <ghetolay@gmail.com>
2018-05-02 15:04:56 +0200

    fix: provide DATE_CONVERTER and make its configurable through forRoot()
    I don't like forRoot() but this will go away with v6 so it's just a matter of days.

--- src/module.ts
+++ src/module.ts
@@ -1,16 +1,18 @@
 import { CommonModule, DatePipe } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { ForMonthOf } from './display/formonth';
 import { ForMonthday } from './display/formonthday';
 import { DayNames, MonthNames, Years } from './display/pipes';
 import { SelectClass } from './display/selectclass';
+import { convertDate } from './index';
 import { MultiSelect } from './selection/multi.select';
 import { RangeSelect } from './selection/range.select';
 import { SingleSelect } from './selection/single.select';
 import { TodayProvider } from './today';
 import { SimpleForOf } from './utils/simplefor';
 import { VALIDATOR_DIRECTIVES } from './validator/directives';
+import { DATE_CONVERTER } from './validator/model';
 
 const decl_exports = [
   SingleSelect,
@@ -31,15 +33,30 @@ const decl_exports = [
   SelectClass
 ];
 
+export interface DatepickerConfig {
+  convertDate?: (v: any) => Date;
+}
+
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: decl_exports,
-  providers: [TodayProvider],
+  providers: [
+    TodayProvider,
+  ],
   exports: [
     ...decl_exports,
     DatePipe
   ]
 })
-export class DatepickerModule { }
+export class DatepickerModule {
+  static forRoot(config?: DatepickerConfig): ModuleWithProviders {
+    return {
+      ngModule: DatepickerModule,
+      providers: [
+        { provide: DATE_CONVERTER, useValue: (config && config.convertDate) || convertDate }
+      ]
+    };
+  }
+}
