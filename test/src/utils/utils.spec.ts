5cde64fefbd04f0bfe96b80365878afd19619bfd
ghetolay <ghetolay@gmail.com>
2018-05-20 23:30:51 +0200

    refactor(test): enable test (was using xdescribe by mistake)

--- test/src/utils/utils.spec.ts
+++ test/src/utils/utils.spec.ts
@@ -2,7 +2,7 @@ import * as moment from 'moment';
 
 import { getIsoWeek } from '../../../src/utils/utils';
 
-xdescribe('getIsoWeek', () => {
+describe('getIsoWeek', () => {
     it('should find same iso week number as momentjs', () => {
         for (let i = 1900; i < 4500; i++) {
             for (let j = 0; j < 12; j++) {
